8d91b1b9d526fa7bf25b2ccadd66c53a16e3dd1c
tanny411 <aysha.kamal7@gmail.com>
2023-04-11 13:39:49 -0400

    run arrange_data.py

--- pyscripts/arrange_data.py
+++ pyscripts/arrange_data.py
@@ -7,8 +7,8 @@ pd.read_csv("data/top_5_matches.csv").to_csv("public/top_5_matches.csv", index=F
 
 for filename in os.scandir("data/topic_models_all_words"):
     file = filename.path
-    pd.read_csv(file).to_csv(f"public/text_label_explorer/{filename}", index=False)
+    pd.read_csv(file).to_csv(f"public/text_label_explorer/{filename.name}", index=False)
 
 for filename in os.scandir("data/data_topics_compressed"):
     file = filename.path
-    pd.read_csv(file).to_csv(f"public/top_topics/top_topics+{filename}", index=False)
+    pd.read_csv(file).to_csv(f"public/top_topics/top_topics+{filename.name}", index=False)
